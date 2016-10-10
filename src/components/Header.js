c70b00758a517b8f3501c3f797dcaf226a055a21
Laurie Williams <lauriewilliamz@gmail.com>
2016-10-11 00:25:47 +1300

    Add basic routing for storefront and storeId in Index.js. Modified Header component to use arrow function instead of React class as the header component is stateless. Add NotFound component for routing 404's.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render() {
-		return (
-			<header className='top'>
-				<h1>Catch 
-					<span className='ofThe'>of</span>
-					<span className='the'>the</span>
-					Day
-				</h1>
-				<h3 className='tagline'><span>{this.props.tagline}</span></h3>
-			</header>
-		);
+//create stateless functional component for Header
+
+const Header = (props) => {
+			return (
+				<header className='top'>
+					<h1>Catch 
+						<span className='ofThe'>of</span>
+						<span className='the'>the</span>
+						Day
+					</h1>
+					<h3 className='tagline'><span>{props.tagline}</span></h3>
+				</header>
+			);
+
 	}
-}
+
+
 
 export default Header;
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+	render() {
+		return (
+			<p> 404 - Sorry this page does not exist! </p>
+			)
+	}
+}
+
+export default NotFound;
