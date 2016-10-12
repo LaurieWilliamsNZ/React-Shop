0fce1b9b176c82a7378e0b88777fda8c68e0ed44
Laurie Williams <enquiries@lauriewilliamsdeveloper.com>
2016-10-13 00:44:56 +1300

    Add order items to the order state when  button clicked. Not currently displayed on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.state = {
       fishes: {},
       order: {} 
@@ -34,6 +35,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder (key) {
+    //copy the state
+    const order = {...this.state.order};
+    //update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    //update the state
+    this.setState({order});
+  }
+
   render () {
     return (
       <div className='catch-of-the-day'>
@@ -43,7 +53,13 @@ class App extends React.Component {
             {
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              .map(key =>
+                <Fish
+                  key={key}
+                  index={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}
+                />)
 
             }
           </ul>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,9 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render () {
-    const { details } = this.props;
+    const { details, index } = this.props;
+    const isAvaliable = details.status === 'available';
+    const buttonText = isAvaliable ? 'Add to Order' : 'Sold Out!';
     return (
       <li className='menu-fish'>
         <img src={details.image} alt={details.name} />
@@ -12,7 +14,9 @@ class Fish extends React.Component {
           <span className='price'>{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button>Add To Order</button>   
+        <button disabled={!isAvaliable} onClick={() => this.props.addToOrder(index)}>
+          {buttonText}
+        </button>   
       </li>
     );
   }
